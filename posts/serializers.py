6b97808ba95f0c4785d14b1ccaf79d199fc71d8b
johnston9 <davejohnston9@gmail.com>
2021-10-20 22:00:15 +0000

    filters

--- posts/serializers.py
+++ posts/serializers.py
@@ -10,6 +10,8 @@ class PostSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     like_id = serializers.SerializerMethodField()
+    comments_count = serializers.ReadOnlyField()
+    likes_count = serializers.ReadOnlyField()
 
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
@@ -42,5 +44,6 @@ class PostSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'title',
             'content', 'image', 'is_owner', 'profile_id',
-            'profile_image', 'image_filter', 'like_id'
+            'profile_image', 'image_filter', 'like_id',
+            'likes_count', 'comments_count',
         ]
